75a4bd0783502863459117a35c2f9c0d319ab531
Boehme, Ingo <ingo.boehme@zuehlke.com>
2022-11-24 09:50:50 +0100

    Created the ToDo story folder with View ViewModel and Model

---
+++ PlaceholderAPISample/Todo/ToDo.swift
@@ -0,0 +1,18 @@
+//
+//  ToDo.swift
+//  PlaceholderAPISample
+//
+//  Created by Ingo Boehme on 24.11.22.
+//
+
+import Foundation
+
+extension TodoView {
+    class Model: ModelProtocol {
+        func fetch(onSuccess: @escaping () -> Void, onError: @escaping (String) -> Void) {
+            //
+        }
+        
+        
+    }
+}
---
+++ PlaceholderAPISample/Todo/Todo.ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  Todo.ViewModel.swift
+//  PlaceholderAPISample
+//
+//  Created by Ingo Boehme on 24.11.22.
+//
+
+import Foundation
+
+extension TodoView {
+    class ViewModel: ViewModelProtocol {
+        func update() {
+            //
+        }
+        
+        
+    }
+}
