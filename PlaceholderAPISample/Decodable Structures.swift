9793f26f2eb5f9d9c5388f526a0bc6de3b1ce546
colinherzog-fhnw <colin.herzog@students.fhnw.ch>
2022-11-24 13:41:30 +0100

    impl: define codable photo struct

--- PlaceholderAPISample/Decodable Structures.swift
+++ PlaceholderAPISample/Decodable Structures.swift
@@ -43,7 +43,13 @@ typealias Albums = [Album]
 
 // MARK: - Photo
 
-struct Photo: Codable {}
+struct Photo: Codable {
+    var albumId: Int
+    var id: Int
+    var title: String
+    var url: String
+    var thumbnailUrl: String
+}
 
 typealias Photos = [Photo]
 
